6a152ab27391060c2177178cbef69de100c65b2d
Master-YEET <christopher.robidas@outlook.com>
2025-03-22 15:03:03 -0400

    Base dialogue system done

---
+++ Scripts/DialogueTrigger.gd
@@ -0,0 +1,11 @@
+extends Interactable
+
+const Ballon = preload("res://user_interfaces/balloon.tscn")
+
+@export var dialogue_ressource: DialogueResource
+@export var dialogue_start: String
+
+func Interact() -> void:
+	var ballon: Node = Ballon.instantiate()
+	get_tree().current_scene.add_child(ballon)
+	ballon.start(dialogue_ressource, dialogue_start)

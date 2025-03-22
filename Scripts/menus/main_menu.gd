2aed37706679de986fd80d87857715c47030a1b4
Master-YEET <christopher.robidas@outlook.com>
2025-03-22 16:01:10 -0400

    Added main menu

---
+++ Scripts/menus/main_menu.gd
@@ -0,0 +1,7 @@
+extends Node
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Levels/dialogue_test_level.tscn")
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
